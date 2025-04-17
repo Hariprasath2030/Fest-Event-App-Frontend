1645de282d793366b0222e018e01d4bb6ca5d86b
Hariprasath2030 <webhari2005@gmail.com>
2025-04-17 14:20:13 +0530

    kjb

--- Fest-book/app/layout.tsx
+++ Fest-book/app/layout.tsx
@@ -1,10 +1,7 @@
-// app/layout.tsx
-'use client'; // <-- This directive marks this as a Client Component
 import { Oxanium } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
 import { ReactNode } from 'react';
 import { AuthProvider } from './organiser/contexts/AuthContext'; // Import the context
-import { BrowserRouter } from 'react-router-dom';
 import './globals.css';
 
 const oxanium = Oxanium({
