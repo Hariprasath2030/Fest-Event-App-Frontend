19492009ccbe16d8dd1e3115be09cb02acc545ac
Hariprasath2030 <webhari2005@gmail.com>
2025-04-18 09:17:11 +0530

    plzzzzzzzzzz

--- Fest-book/app/layout.tsx
+++ Fest-book/app/layout.tsx
@@ -1,17 +1,17 @@
 import { Oxanium } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
 import { ReactNode } from 'react';
-import { AuthProvider } from './organiser/contexts/AuthContext'; // Import the context
+import { AuthProvider } from './organiser/contexts/AuthContext';
 import './globals.css';
 
 const oxanium = Oxanium({
   subsets: ['latin'],
 });
 
-const clerkPublishableKey = "pk_test_cmVhZHktd2FzcC0yNi5jbGVyay5hY2NvdW50cy5kZXYk";
+const clerkPublishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
 if (!clerkPublishableKey) {
-  throw new Error('Add your Clerk Publishable Key to the .env file')
+  throw new Error('Missing Clerk Publishable Key. Set NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY in .env.local');
 }
 
 export const metadata = {
@@ -29,7 +29,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <html lang="en">
         <head />
         <body className={oxanium.className}>
-          {/* Wrapping children in necessary providers */}
           <AuthProvider>
             {children}
           </AuthProvider>
